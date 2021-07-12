5733014a044c087ba87a2d75e324deebe0c511b1
saicharan67 <mahankalisaicharan@gmail.com>
2021-07-12 09:57:42 +0530

    Changed layout

--- index.js
+++ index.js
@@ -8,24 +8,25 @@ const clear =require('clear')
 clear()
 
 const data = {
-    name: chalk.bold.green("                    Mahankali Sai Charan"),
+    name: chalk.bold.green("              Mahankali Sai Charan"),
     handle: chalk.white("@saicharan67"),
     work: `${chalk.white("Student at")} ${chalk
-        .hex("#2b82b2")
-        .bold("NIT Andhra")}`,
+        .bold
+        .blue
+        ("NIT Andhra")}`,
     
-    github: chalk.gray("https://github.com/") + chalk.green("saicharan67"),
-    LeetCode: chalk.gray("https://leetcode.com/") + chalk.cyan("MaC-02"),
+    github: chalk.gray("https://github.com/") + chalk.blue("saicharan67"),
+    LeetCode: chalk.gray("https://leetcode.com/") + chalk.blue("MaC-02"),
     linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("saicharan67"),
-    web: chalk.cyan("https://saicharan67.github.io/PortFolio"),
-    npx: chalk.red("npx") + " " + chalk.white("saicharan"),
+    web: chalk.blue("https://saicharan67.github.io/PortFolio"),
+    npx: chalk.hex("#FDFF62")("npx") + " " + chalk.white("saicharan"),
 
-    labelWork: chalk.white.bold("       Work:"),
-    labelLeetcode: chalk.white.bold("    Leetcode:"),
-    labelGitHub: chalk.white.bold("     GitHub:"),
-    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
-    labelWeb: chalk.white.bold("        Web:"),
-    labelCard: chalk.white.bold("       Card:")
+    labelWork: chalk.white.bold("          Work:"),
+    labelLeetcode: chalk.white.bold("  Leetcode:"),
+    labelGitHub: chalk.white.bold("    GitHub:"),
+    labelLinkedIn: chalk.white.bold("  LinkedIn:"),
+    labelWeb: chalk.white.bold("   Website:"),
+    labelCard: chalk.white.bold("              Card:")
 };
 
 const Message = boxen(
@@ -41,19 +42,23 @@ const Message = boxen(
         `${data.labelWeb}  ${data.web}`,
         ``,
         `${data.labelCard}  ${data.npx}`,
-        ``,
-        `${chalk.italic(
-            "I am currently looking for new opportunities,"
-        )}`,
+        ``
 
     ].join("\n"),
     {
-        margin: 1,
         float: 'center',
         padding: 1,
-        borderStyle: "single",
-        borderColor: "green"
+        borderStyle: "double",
+        borderColor: "yellow"
     }
 );
 
 console.log(Message)
+
+const tip = [
+    `${chalk.bold("Tip")}: Try ${chalk.cyan.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
